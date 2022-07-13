ac663da406833ee792fbb61f49ae561ea72fe51e
lnoueryo <jounetsism@gmail.com>
2022-07-14 00:54:04 +0900

    ヘッド追加

--- main.go
+++ main.go
@@ -20,61 +20,96 @@ import (
 )
 
 func main() {
-	index := 1
-	cityCode := "11201"
-	fmt.Println(fmt.Sprintf("city code %s", cityCode))
+	prefectureCode := 13
+	areaCode := 101
 	for {
-		fmt.Println(fmt.Sprintf("page %s start", strconv.Itoa(index)))
-		webPage := fmt.Sprintf("https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=11&sc=%s&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=25&pc=50&page=%s", cityCode, strconv.Itoa(index))
-		resp, err := http.Get(webPage)
-		if err != nil {
-			log.Printf("failed to get html: %s", err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 200 {
-			log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
-		}
-
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			log.Printf("failed to load html: %s", err)
-		}
-
-		detailURLs := []string{}
-		doc.Find("div.cassetteitem a").Each(func(i int, s *goquery.Selection) {
-			band, ok := s.Attr("href")
-			if ok {
-				url := strings.Replace(band, "javascript:void(0);", "", -1)
-				if url != "" {
-					detailURLs = append(detailURLs, url)
-				}
+		index := 1
+		cityCode := strconv.Itoa(prefectureCode) + strconv.Itoa(areaCode)
+		fmt.Println(fmt.Sprintf("city code %s", cityCode))
+		for {
+			fmt.Println(fmt.Sprintf("page %s start", strconv.Itoa(index)))
+			webPage := fmt.Sprintf("https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=%s&sc=%s&cb=0.0&ct=9999999&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=25&pc=50&page=%s", strconv.Itoa(prefectureCode), cityCode, strconv.Itoa(index))
+			resp, err := http.Get(webPage)
+			if err != nil {
+				log.Printf("failed to get html: %s", err)
 			}
-		})
-
-		if len(detailURLs) == 0 {
+			defer resp.Body.Close()
+	
+			if resp.StatusCode != 200 {
+				log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
+			}
+	
+			doc, err := goquery.NewDocumentFromReader(resp.Body)
+			if err != nil {
+				log.Printf("failed to load html: %s", err)
+			}
+	
+			detailURLs := []string{}
+			log.Println("start to get URLs")
+			doc.Find("div.cassetteitem a").Each(func(i int, s *goquery.Selection) {
+				band, ok := s.Attr("href")
+				if ok {
+					url := strings.Replace(band, "javascript:void(0);", "", -1)
+					if url != "" {
+						detailURLs = append(detailURLs, url)
+					}
+				}
+			})
+	
+			if len(detailURLs) == 0 {
+				log.Println("nothing more")
+				break
+			}
+	
+			log.Println("start to get records")
+			records := [][]string{}
+			for _, url := range detailURLs {
+				texts := getDetail(url, cityCode)
+				if len(texts) == 0 {
+					continue
+				}
+				records = append(records, texts)
+				log.Println(texts)
+			}
+	
+			log.Println("start to write records")
+			filename := cityCode + ".csv"
+			f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+	
+			w := csv.NewWriter(f)
+	
+			if index == 1 {
+				head := []string{
+					"prefecture_code",
+					"city_code",
+					"price",
+					"management_fee",
+					"security_deposit",
+					"key_money",
+					"prefecture",
+					"city",
+					"station",
+					"address",
+					"layout",
+					"area",
+					"age",
+					"floor",
+				}
+				w.Write(head)
+			}
+			w.WriteAll(records)
+	
+			w.Flush()
+			index += 1
+		}
+		if areaCode == 700 {
+			areaCode = 101
 			break
 		}
-
-		records := [][]string{}
-		for _, url := range detailURLs {
-			texts := getDetail(url)
-			if len(texts) == 0 {
-				continue
-			}
-			records = append(records, texts)
-		}
-		f, err := os.OpenFile("file.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		w := csv.NewWriter(f)
-
-		w.WriteAll(records)
-
-		w.Flush()
-		index += 1
+		areaCode += 1
 	}
 	// f, _ := os.Open("./users.csv")
 	// defer f.Close()
@@ -92,8 +127,8 @@ func main() {
 	//   }
 }
 
-func getDetail(path string) ([]string) {
-	tds := []string{}
+func getDetail(path string, cityCode string) ([]string) {
+	tds := []string{cityCode[:2], cityCode}
 	url := "https://suumo.jp" + path
 	resp, err := http.Get(url)
 	if err != nil {
@@ -115,30 +150,51 @@ func getDetail(path string) ([]string) {
 
 	doc.Find("div.property_view_note-info").Each(func(i int, s *goquery.Selection) {
 		s.Find("span").Each(func(i int, s *goquery.Selection) {
-			conditions := []string{
-				"管理費・共益費:",
-				"万円",
-				"円",
-				"敷金:",
-				"礼金:",
-				"保証金:",
-				"敷引・償却:",
+			if i < 4 {
+				conditions := []string{
+					"管理費・共益費:",
+					"万円",
+					"円",
+					"敷金:",
+					"礼金:",
+				}
+				text := s.Text()
+				for _, condition := range conditions {
+					text = strings.Replace(text, condition, "", -1)
+				}
+				text = strings.TrimSpace(text)
+				if text == "-" {
+					text = "0"
+				}
+				tds = append(tds, text)
 			}
-			text := s.Text()
-			for _, condition := range conditions {
-				text = strings.Replace(text, condition, "", -1)
-			}
-			text = strings.TrimSpace(text)
-			if text == "-" {
-				text = "0"
-			}
-			tds = append(tds, text)
 		})
 	})
+
+	doc.Find("div.left_column ul li").Each(func(i int, s *goquery.Selection) {
+		if i > 1 && i < 5 {
+			a := s.Find("a")
+			tds = append(tds, a.Text())
+		}
+	})
+
 	doc.Find("table.property_view_table").Each(func(i int, s *goquery.Selection) {
 		s.Find("td").Each(func(i int, s *goquery.Selection) {
-			if i != 1 {
-				tds = append(tds, s.Text())
+			if i != 1 && i < 7 {
+				text := s.Text()
+				if text == "新築" {
+					text = "0"
+				}
+				conditions := []string{
+					"m2",
+					"築",
+					"年",
+					"階",
+				}
+				for _, condition := range conditions {
+					text = strings.Replace(text, condition, "", -1)
+				}
+				tds = append(tds, text)
 			}
 		})
 	})
